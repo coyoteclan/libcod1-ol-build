23246a1690b09320b1322a26a563c33f2f5f465b
Mechamorph <118144070+coyoteclan@users.noreply.github.com>
2025-01-22 11:51:00 +0000

    update script

--- doit.sh
+++ doit.sh
@@ -10,6 +10,6 @@ cp -rf /workspaces/libcod1-ol-build/modified-src-files/* ~/lc-build/libcod1/src/
 ./build.sh "$@"
 
 cd /workspaces/libcod1-ol-build
-BUILD_DIR="build_$(date +%Y-%m-%d_%H-%M-%S)"
+BUILD_DIR="builds/build_$(date +%Y-%m-%d_%H-%M-%S)"
 mkdir $BUILD_DIR
 cp ~/lc-build/libcod1/bin/libcod1.so ./$BUILD_DIR
